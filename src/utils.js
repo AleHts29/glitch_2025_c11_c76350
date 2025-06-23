991673c4ac78eca72a9a0624c6fff0ee2ec27721
Alejandro Huertas <alejo29h@gmail.com>
2025-06-23 20:39:13 -0300

    sync

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,5 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import multer from 'multer';
 
 /**
 *
@@ -18,30 +17,3 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 export default __dirname;
-
-
-
-
-// Configuracion MULTER
-const storage = multer.diskStorage({
-    // Ubicacion de la ruta donde voy a persister los archivos
-    destination: function (req, file, cb) {
-        cb(null, `${__dirname}/public/img`)
-    },
-
-
-    // renombre de los archivos
-    filename: function (req, file, cb) {
-        cb(null, `${Date.now()}_${file.originalname}`)
-    }
-})
-
-
-export const uploader = multer({
-    storage,
-    // si se genera algun error, lo capturamos
-    onError: function (err, next) {
-        console.log(err);
-        next()
-    }
-})
